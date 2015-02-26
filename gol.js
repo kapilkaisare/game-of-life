1d95126b2543fa1d7781727190dc342d71bffa93
Kapil Kaisare <kapil@kaisare.in>
2015-02-26 20:47:24 +0530

    Rendering the world.

--- gol.js
+++ gol.js
@@ -31,23 +31,84 @@ var WorldConfiguration = (function (){
 			_readRowCount();
 			_readColCount();
 		},
-		_init = function () {
-			document.getElementById(useBtnId).onclick = _loadCounts;
+		_init = function (listeners) {
+			document.getElementById(useBtnId).onclick = listeners.useClick;
 		};
 
 	return {
 		readRowCount: _readRowCount,
 		readColCount: _readColCount,
+		loadCounts: _loadCounts,
 		getRows: _getRows,
 		getCols: _getCols,
 		init: _init
 	};
 })();
 
+var World = (function () {
+	return function () {
+		var
+			renderContainerId = 'world-container',
+			constructCellId = function (row, col) {
+				return 'cell_' + row + '_' + col;
+			},
+			constructRowId = function (row) {
+				return 'row_' + row;
+			},
+			renderCell = function (row, col) {
+				var cell = document.createElement('td');
+				cell.setAttribute('id', constructCellId(row, col));
+				cell.style.padding = '5px';
+				cell.style.border = 'solid 1px';
+				return cell;
+			},
+			renderRow = function (row, col) {
+				var rowEl = document.createElement('tr');
+				rowEl.setAttribute('id', constructRowId(row));
+				for (var i = 1; i <= col; i++) {
+					rowEl.appendChild(renderCell(row, i));
+				}
+				return rowEl;
+			};
+
+		this.renderWorld = function (rows, cols) {
+			var
+				table = document.createElement('table'),
+				worldHolder = document.getElementById(renderContainerId);
+
+			while (worldHolder.firstChild) {
+				worldHolder.removeChild(worldHolder.firstChild);
+			}
+
+			for (var i = 1; i <= rows; i++) {
+				table.appendChild(renderRow(i, cols));
+			}
+
+			table.style.padding = '5px';
+			table.style.border = 'solid 1px';
+
+			worldHolder.appendChild(table);
+			
+		};
+
+		return this;
+	};
+})();
+
 var Gol = (function (){
 	var
 		_run = function (config, worldContainer) {
-			config.init();
+			var
+				onUseClick = function () {
+					var world = new World();
+					config.loadCounts();
+					world.renderWorld(config.getRows(), config.getCols());
+				},
+				listeners = {
+					useClick: onUseClick
+				};
+
+			config.init(listeners);
 		};
 
 	return {
